b29bc77316cba8f4a12aa6a26ccf3ec038b98cc3
urkebb <urkebb@elfak.rs>
2021-01-07 18:36:53 +0100

    aa

--- routes/posts-routes.js
+++ routes/posts-routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log('GET Request in Places');
+  console.log('GET Request in Posts');
   res.json({message: 'It works!'});
 });
 
