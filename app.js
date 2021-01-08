5527b2d5c0ec034bac11fdb77c808d0a03ae2c70
urkebb <urkebb@elfak.rs>
2021-01-08 19:53:01 +0100

    dodati errori

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const postsRoutes = require('./routes/posts-routes');
 
 const app = express();
 
-app.use(postsRoutes);
+app.use('/api/posts', postsRoutes); // => /api/posts...
+
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+    res.status(error.code || 500)
+    res.json({ message: error.message || 'Doslo je do nepoznate greske' });
+});
 
 app.listen(5000);
--- routes/posts-routes.js
+++ routes/posts-routes.js
@@ -2,9 +2,44 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('GET Request in Posts');
-  res.json({message: 'It works!'});
+const DUMMY_POSTS = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    creator: 'u1'
+  }
+];
+
+router.get('/:pid', (req, res, next) => {
+  const postId = req.params.pid; // { pid: 'p1' }
+  const post = DUMMY_POSTS.find(p => {
+    return p.id === postId;
+  });
+
+  if (!post) {
+    const error = new Error('Ne mogu da pronadjem post za navedeni ID');
+    error.code = 404;
+    throw error;
+  }
+
+  res.json({ post }); // => { place } => { place: place }
+});
+
+router.get('/user/:uid', (req, res, next) => {
+  const userId = req.params.uid;
+
+  const post = DUMMY_POSTS.find(p => {
+    return p.creator === userId;
+  });
+
+  if (!post) {
+    const error = new Error('Ne mogu da pronadjem post za navedeni korisnikov ID');
+    error.code = 404;
+    return next(error);
+  }
+
+  res.json({ post });
 });
 
 module.exports = router;
