818b0ca52b56428e024eb66deaae26eb8ff57891
kopriva98 <66503247+kopriva98@users.noreply.github.com>
2021-01-18 19:36:20 +0100

    Accepting image
    Multer

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { check } = require('express-validator');
 const usersController=require('../controllers/user-controllers');
+const fileUpload = require('../MIddleware/file-upload')
  
 const router = express.Router();
 
 router.get('/',usersController.getUsers);
 
-router.post('/signup',[
+router.post('/signup',fileUpload.single('image'),[
    check('name').not().isEmpty(),check('email').normalizeEmail().isEmail(),check('password').isLength({min: 6})
 ],usersController.signup);
 
